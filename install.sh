f085cee04570888d393f72a4c9b23be6feb85059
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-05-29 19:02:07 +0700

    add zsh dwm and slstatus

--- install.sh
+++ install.sh
@@ -10,13 +10,30 @@ cd ..
 rm -rf yay-git
 
 yay -Syu --noconfirm
-yay -S dwm imlib2 --noconfirm
+yay -S dwm imlib2 zsh --noconfirm
 
 git clone https://github.com/LunarVim/LunarVim.git
 cd LunarVim
 echo -e "y\ny\ny" | make install
 cd ..
 rm -rf LunarVim
-echo "export PATH=\$HOME/.local/bin:\$PATH" >> $HOME/.zshrc
 echo "export PATH=\$HOME/.local/bin:\$PATH" >> $HOME/.bashrc
+echo "
+export PATH=\$HOME/.local/bin:\$PATH
+HISTFILE=~/.histfile
+HISTSIZE=1000
+bindkey -v" >> $HOME/.zshrc
+
 export PATH=$HOME/.local/bin:$PATH
+
+mkdir ~/.dwm
+echo "slstatus &" > ~/.dwm/autostart.sh
+mkdir ~/theme
+cd ~/theme
+git clone https://github.com/0x73hahd/dwm-config
+cd dwm-config/source
+make
+sudo make install
+cd ../slstatus
+make
+sudo make install
