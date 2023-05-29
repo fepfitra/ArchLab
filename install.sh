00237b37f50064f705690b5af917ed60ee750990
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-05-29 15:20:03 +0700

    fing wrong rc format

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ make install
 sudo make install
 cd ..
 rm -rf LunarVim
-echo export PATH=$HOME/.local/bin > $HOME/.zshrc
-echo export PATH=$HOME/.local/bin > $HOME/.bashrc
-export PATH=$HOME/.local/bin
+echo "export PATH=\$HOME/.local/bin:\$PATH" >> $HOME/.zshrc
+echo "export PATH=\$HOME/.local/bin:\$PATH" >> $HOME/.bashrc
+export PATH=$HOME/.local/bin:$PATH
