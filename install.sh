155bf6f2dcd5bce4341f7c9a4751c5fd5ef8f24a
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-05-29 17:48:10 +0700

    reconstruct order

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
-sudo -S pacman -Syu --noconfirm
-sudo -S pacman -S neovim python3 cargo --noconfirm
+sudo pacman -Syu --noconfirm
+sudo pacman neovim python3 cargo go sddm --noconfirm
 cd ~
+
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git
+echo -e "y" | makepkg -si
+cd ..
+rm -rf yay-git
+
+yay -Syu --noconfirm
+yay -S dwm --noconfirm
+
 git clone https://github.com/LunarVim/LunarVim.git
 cd LunarVim
 echo -e "y\ny\ny" | make install
@@ -9,15 +19,3 @@ rm -rf LunarVim
 echo "export PATH=\$HOME/.local/bin:\$PATH" >> $HOME/.zshrc
 echo "export PATH=\$HOME/.local/bin:\$PATH" >> $HOME/.bashrc
 export PATH=$HOME/.local/bin:$PATH
-
-sudo -S pacman -S go --noconfirm
-git clone https://aur.archlinux.org/yay-git.git
-cd yay-git
-echo -e "y" | makepkg -si
-cd ..
-rm -rf yay-git
-
-yay -Syu --noconfirm
-
-sudo -S pacman -S sddm --noconfirm
-yay -S dwm --noconfirm
