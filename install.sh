0c8de5b9bc432b17d0d8ec1c6a0d0cf068ea9c69
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-05-29 15:29:08 +0700

    install LunarVim without comfirmation

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ sudo pacman -S neovim python3 cargo --noconfirm
 cd ~
 git clone https://github.com/LunarVim/LunarVim.git
 cd LunarVim
-make install
+echo -e "y\ny\ny" | make install
 cd ..
 rm -rf LunarVim
 echo "export PATH=\$HOME/.local/bin:\$PATH" >> $HOME/.zshrc
@@ -14,3 +14,5 @@ cd yay-git
 makepkg -si
 cd ..
 rm -rf yay-git
+
+sudo pacman -S sddm --noconfirm
