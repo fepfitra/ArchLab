4e6a7acfb986c3d0cc295fe4332add99cacdf291
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-05-29 15:23:46 +0700

    remove intall LunarVim to root

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ cd ~
 git clone https://github.com/LunarVim/LunarVim.git
 cd LunarVim
 make install
-sudo make install
 cd ..
 rm -rf LunarVim
 echo "export PATH=\$HOME/.local/bin:\$PATH" >> $HOME/.zshrc
