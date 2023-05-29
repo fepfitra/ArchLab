80346759e50758a0ad09dd9c783cc880e7de6c44
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-05-29 15:23:24 +0700

    installing yay

--- install.sh
+++ install.sh
@@ -9,3 +9,9 @@ rm -rf LunarVim
 echo "export PATH=\$HOME/.local/bin:\$PATH" >> $HOME/.zshrc
 echo "export PATH=\$HOME/.local/bin:\$PATH" >> $HOME/.bashrc
 export PATH=$HOME/.local/bin:$PATH
+
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git
+makepkg -si
+cd ..
+rm -rf yay-git
